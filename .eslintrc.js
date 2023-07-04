c3141ce063fa50957117e9b4c09b36e3e5266f63
Tim Brust <github@timbrust.de>
2023-07-04 12:55:27 +0000

    refactor: replace TSLint with ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    root: true,
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  };
+  
--- dist/www/delete-launch-screen-cache.js
+++ dist/www/delete-launch-screen-cache.js
@@ -5,7 +5,7 @@ var DeleteLaunchScreenCache = (function () {
     }
     DeleteLaunchScreenCache.deleteLaunchScreenCache = function () {
         return new Promise(function (resolve, reject) {
-            cordova_1.exec(resolve, reject, 'DeleteLaunchScreenCache', 'deleteLaunchScreenCache', []);
+            (0, cordova_1.exec)(resolve, reject, 'DeleteLaunchScreenCache', 'deleteLaunchScreenCache', []);
         });
     };
     return DeleteLaunchScreenCache;
