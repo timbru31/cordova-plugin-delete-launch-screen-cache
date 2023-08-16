3ffd714ecf04ad0454f7332afda7574a7b7afa2d
Tim Brust <github@timbrust.de>
2023-08-16 10:13:32 +0200

    build: update deps and enable auto merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
-    root: true,
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  };
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  root: true,
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  parserOptions: {
+    project: 'tsconfig.json'
+  }
+};
